1bb1942c29060d1dd567e9ccdec7180a357d4186
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2023-12-04 23:36:57 +0800

    leetcode 0020

---
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0020.java
@@ -0,0 +1,34 @@
+package com.liangkuncao.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class LeetCode0020 {
+    public boolean isValid(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        } else if (s.length() % 2 == 1) {
+            return false;
+        }
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!stack.isEmpty() && map.get(stack.peek()).equals(ch)) {
+                stack.pop();
+            } else {
+                if (!map.containsKey(ch)) {
+                    return false;
+                }
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+
+    }
+}
