3be77198041d7dbcffb63762b843d4b6244d7441
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2023-12-05 08:24:28 +0800

    leetcode 0141

---
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0141.java
@@ -0,0 +1,30 @@
+package com.liangkuncao.leetcode;
+
+public class LeetCode0141 {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
