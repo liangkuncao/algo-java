60417e1aa528cb24765c5119d8b80dfe0a449828
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2023-12-08 08:21:59 +0800

    leetcode 0100

---
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0100.java
@@ -0,0 +1,26 @@
+package com.liangkuncao.leetcode;
+
+import com.liangkuncao.leetcode.common.TreeNode;
+
+public class LeetCode0100 {
+    /**
+     * 题型：二叉树
+     * 解法：DFS，递归
+     * 时间复杂度：O（N）
+     * 空间复杂度：O（二叉树的高度，最坏情况下是N）
+     * @param p
+     * @param q
+     * @return
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else {
+            return p.val == q.val
+                    && isSameTree(p.left, q.left)
+                    && isSameTree(p.right, q.right);
+        }
+    }
+}
--- leetcode/src/com/liangkuncao/leetcode/LeetCode0104.java
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0104.java
@@ -2,6 +2,12 @@ package com.liangkuncao.leetcode;
 
 import com.liangkuncao.leetcode.common.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class LeetCode0104 {
     /**
      * 题型：二叉树
@@ -18,5 +24,30 @@ public class LeetCode0104 {
         int leftDepth = maxDepth(root.left);
         int rightDepth = maxDepth(root.right);
         return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+    public int maxDepth_BFS(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> nodes = new LinkedList<>() {
+        };
+        nodes.offer(root);
+        int result = 0;
+        while (!nodes.isEmpty()) {
+            int size = nodes.size();
+            result++;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = nodes.poll();
+                if (node.left != null) {
+                    nodes.offer(node.left);
+                }
+                if (node.right != null) {
+                    nodes.offer(node.right);
+                }
+            }
+
+        }
+        return result;
     }
 }
