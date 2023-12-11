b54f6bf0362c5a57c66fab673e7e941a3791351b
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2023-12-11 21:35:38 +0800

    leetcode 0112

---
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0112.java
@@ -0,0 +1,16 @@
+package com.liangkuncao.leetcode;
+
+import com.liangkuncao.leetcode.common.TreeNode;
+
+public class LeetCode0112 {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+        return hasPathSum(root.left, targetSum - root.val)
+                || hasPathSum(root.right, targetSum - root.val);
+    }
+}
