997b931e1485fb26408c1858600a537b7de0ef0e
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2023-12-15 00:16:15 +0800

    leetcode 0108

---
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0108.java
@@ -0,0 +1,20 @@
+package com.liangkuncao.leetcode;
+
+import com.liangkuncao.leetcode.common.TreeNode;
+
+public class LeetCode0108 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int mid = (start + end + 1) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = sortedArrayToBST(nums, start, mid - 1);
+        root.right = sortedArrayToBST(nums, mid + 1, end);
+        return root;
+    }
+}
