5409634070acaee8cb4f7df2631cfce4441d7c20
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2023-12-16 08:28:32 +0800

    leetcode 0021

--- leetcode/src/com/liangkuncao/leetcode/LeetCode0021.java
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0021.java
@@ -1,5 +1,7 @@
 package com.liangkuncao.leetcode;
 
+import com.liangkuncao.leetcode.common.ListNode;
+
 /*
 题型：链表
 解决方案：迭代
@@ -9,6 +11,15 @@ package com.liangkuncao.leetcode;
 其他方案：递归
  */
 public class LeetCode0021 {
+    /**
+     * 题型：表
+     * 解法：迭代
+     * 时间复杂度：O（N）
+     * 空间复杂度：O（1）
+     * @param list1
+     * @param list2
+     * @return
+     */
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(0);
         ListNode cur = dummy;
@@ -29,6 +40,31 @@ public class LeetCode0021 {
         }
 
         return dummy.next;
+    }
+
+    /**
+     * 题型：表
+     * 解法：递归
+     * 时间复杂度：O（N）
+     * 空间复杂度：O（N）
+     * @param list1
+     * @param list2
+     * @return
+     */
+    public ListNode mergeTwoLists_recursion(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null) {
+            return null;
+        } else if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        } else if (list1.val <= list2.val) {
+            list1.next = mergeTwoLists_recursion(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists_recursion(list1, list2.next);
+            return list2;
+        }
     }
 }
 
