107590dd46eba7805435621573314c084fb4b621
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2024-01-26 19:13:48 +0800

    leetcode 0018

--- leetcode/src/com/liangkuncao/leetcode/LeetCode0018.java
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0018.java
@@ -43,4 +43,54 @@ public class LeetCode0018 {
         }
         return result;
     }
+
+    public List<List<Integer>> fourSum2(int[] nums, int target) {
+        int length = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if ((long)nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break;
+            }
+            if ((long)nums[i] + nums[length - 3] + nums[length - 2] + nums[length - 1] < target) {
+                continue;
+            }
+
+            for (int j = i + 1; j < length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                    break;
+                }
+                if ((long) nums[i] + nums[j] + nums[length - 2] + nums[length - 1] < target) {
+                    continue;
+                }
+                int left = j + 1;
+                int right = length - 1;
+                while (left < right) {
+                    long total = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if (total == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        left++;
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        right--;
+                    } else if (total < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
 }
