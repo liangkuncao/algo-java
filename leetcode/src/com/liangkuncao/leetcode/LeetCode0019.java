2bbae6ab36694195c7d639b13374fcf7a99d2fe7
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2024-03-23 18:36:39 +0800

    leetcode 0019

--- leetcode/src/com/liangkuncao/leetcode/LeetCode0019.java
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0019.java
@@ -5,16 +5,16 @@ import com.liangkuncao.leetcode.common.ListNode;
 public class LeetCode0019 {
     /**
      * 题型：单链表
-     * 解法：遍历
+     * 解法：遍历计算表的长度
      * 时间复杂度：O（N）
      * 空间复杂度：O（1）
+     *
      * @param head
      * @param n
      * @return
      */
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
+        ListNode dummy = new ListNode(-1, head);
         int size = 0;
         ListNode cur = dummy;
         while (cur.next != null) {
@@ -26,6 +26,29 @@ public class LeetCode0019 {
             cur = cur.next;
         }
         cur.next = cur.next.next;
+        return dummy.next;
+    }
+
+    /**
+     * 解法：双指针
+     * 时间复杂度：O（N）
+     * 空间复杂度：O（1）
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1, head);
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
         return dummy.next;
     }
 }
