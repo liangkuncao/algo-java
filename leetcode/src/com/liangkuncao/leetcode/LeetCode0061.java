b21c60367ab7d8a05eccdc4e5fce60697412910a
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2024-03-28 22:06:33 +0800

    leetcode 0061

--- leetcode/src/com/liangkuncao/leetcode/LeetCode0061.java
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0061.java
@@ -2,12 +2,14 @@ package com.liangkuncao.leetcode;
 
 import com.liangkuncao.leetcode.common.ListNode;
 
+
 public class LeetCode0061 {
     /**
      * 题型：单链表
      * 解法：计算表长度
      * 时间复杂度：O（N）
      * 空间复杂度：O（1）
+     *
      * @param head
      * @param k
      * @return
@@ -39,5 +41,41 @@ public class LeetCode0061 {
         newTail.next = null;
         tail.next = head;
         return newHead;
+    }
+
+    public ListNode rotateRight2(ListNode head, int k) {
+        int length = getLength(head);
+        if (length == 0) {
+            return head;
+        }
+        k %= length;
+        if (k == 0) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        for (int i = 0; i < k; i++) {
+            fast = fast.next;
+        }
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        ListNode newHead = slow.next;
+        slow.next = null;
+        fast.next = head;
+        return newHead;
+    }
+
+    private int getLength(ListNode head) {
+        if (head == null) {
+            return 0;
+        }
+        int count = 0;
+        while (head != null) {
+            head = head.next;
+            count++;
+        }
+        return count;
     }
 }
