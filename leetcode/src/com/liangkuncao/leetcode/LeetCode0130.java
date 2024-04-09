6c97b0870127a6e8e1a3f47d57b7822f2d2be044
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2024-04-09 21:46:17 +0800

    leetcode 0130

--- leetcode/src/com/liangkuncao/leetcode/LeetCode0130.java
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0130.java
@@ -1,48 +1,43 @@
 package com.liangkuncao.leetcode;
 
 public class LeetCode0130 {
-    private final char tmp = '1';
+    private static final char TEMP = 'T';
+    private static final char LAND = 'O';
+    private static final char WATER = 'X';
+
     public void solve(char[][] board) {
         int m = board.length;
         int n = board[0].length;
         for (int x = 0; x < m; x++) {
-            if (board[x][0] == 'O') {
-                changeValue(board, x, 0);
-            }
-            if (board[x][n - 1] == 'O') {
-                changeValue(board, x, n - 1);
-            }
+            dfs(board, x, 0);
+            dfs(board, x, n - 1);
         }
         for (int y = 0; y < n; y++) {
-            if (board[0][y] == 'O') {
-                changeValue(board, 0, y);
-            }
-            if (board[m - 1][y] == 'O') {
-                changeValue(board, m - 1, y);
-            }
+            dfs(board, 0, y);
+            dfs(board, m - 1, y);
         }
         for (int x = 0; x < m; x++) {
             for (int y = 0; y < n; y++) {
-                if (board[x][y] == 'O') {
-                    board[x][y] = 'X';
-                } else if (board[x][y] == tmp) {
-                    board[x][y] = 'O';
+                if (board[x][y] == LAND) {
+                    board[x][y] = WATER;
+                } else if (board[x][y] == TEMP) {
+                    board[x][y] = LAND;
                 }
             }
         }
     }
 
-    private void changeValue(char[][] board, int x, int y) {
+    private void dfs(char[][] board, int x, int y) {
         if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) {
             return;
         }
-        if (board[x][y] != 'O') {
+        if (board[x][y] != LAND) {
             return;
         }
-        board[x][y] = tmp;
-        changeValue(board, x, y - 1);
-        changeValue(board, x - 1, y);
-        changeValue(board, x, y + 1);
-        changeValue(board, x + 1, y);
+        board[x][y] = TEMP;
+        dfs(board, x, y - 1);
+        dfs(board, x - 1, y);
+        dfs(board, x, y + 1);
+        dfs(board, x + 1, y);
     }
 }
