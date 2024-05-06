9095626bb20f73622a5b1ca73f078988e3ed6ef1
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2024-05-06 21:48:20 +0800

    leetcode 0108

--- leetcode/src/com/liangkuncao/leetcode/LeetCode0108.java
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0108.java
@@ -3,18 +3,24 @@ package com.liangkuncao.leetcode;
 import com.liangkuncao.leetcode.common.TreeNode;
 
 public class LeetCode0108 {
+    /**
+     * 题型：二叉树
+     * 题解：分治
+     * @param nums
+     * @return
+     */
     public TreeNode sortedArrayToBST(int[] nums) {
-        return sortedArrayToBST(nums, 0, nums.length - 1);
+        return help(nums, 0, nums.length - 1);
     }
 
-    public TreeNode sortedArrayToBST(int[] nums, int start, int end) {
-        if (start > end) {
+    private TreeNode help(int[] nums, int left, int right) {
+        if (left > right) {
             return null;
         }
-        int mid = (start + end + 1) / 2;
-        TreeNode root = new TreeNode(nums[mid]);
-        root.left = sortedArrayToBST(nums, start, mid - 1);
-        root.right = sortedArrayToBST(nums, mid + 1, end);
-        return root;
+        int mid = (left + right) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = help(nums, left, mid - 1);
+        node.right = help(nums, mid + 1, right);
+        return node;
     }
 }
