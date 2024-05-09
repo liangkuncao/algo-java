17bf660bae1ea18837f263b7545d8c4cb0fe0d0d
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2024-05-09 22:23:20 +0800

    leetcode 0077

--- leetcode/src/com/liangkuncao/leetcode/LeetCode0077.java
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0077.java
@@ -6,15 +6,26 @@ import java.util.List;
 public class LeetCode0077 {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
-        int[] nums = new int[n];
-        for (int i = 0; i < n; i++) {
-            nums[i] = i + 1;
-        }
-        dfs(nums, new int[k], 0, k);
-        return null;
+        List<Integer> current = new ArrayList<>();
+        help(current, result, 1, n, k);
+        return result;
     }
 
-    private void dfs(int[] nums, int[] ints, int i, int k) {
-        
+    private void help(List<Integer> current, List<List<Integer>> result, int start, int end, int k) {
+        if (k <= 0) {
+            result.add(List.copyOf(current));
+            return;
+        }
+        for (int i = start; i <= end; i++) {
+            current.add(i);
+            help(current, result, i + 1, end, k - 1);
+            current.remove(current.size() - 1);
+        }
     }
+
+    public static void main(String[] args) {
+        LeetCode0077 leetCode0077 = new LeetCode0077();
+        leetCode0077.combine(4, 2);
+    }
+
 }
