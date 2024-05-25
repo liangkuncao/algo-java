6bcb8cdf40d0aeb3346472d3a761f957e70cf5b1
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2024-05-26 07:51:23 +0800

    leetcode 0105

--- leetcode/src/com/liangkuncao/leetcode/LeetCode0105.java
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0105.java
@@ -1,27 +1,32 @@
 package com.liangkuncao.leetcode;
 
-import com.liangkuncao.leetcode.common.TreeNode;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class LeetCode0105 {
-    int[][] directions = new int[][] {
+    int[][] directions = new int[][]{
             {-1, 0}, {0, 1}, {1, 0}, {0, -1}
     };
+
     public int maxAreaOfIsland(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         int result = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    int area = count(grid, i, j);
-                    result = Math.max(result, area);
-                }
+                result = Math.max(result, dfs(grid, i, j));
             }
         }
         return result;
     }
 
-    private int count(int[][] grid, int x, int y) {
+    private int dfs(int[][] grid, int x, int y) {
+        if (x < 0 || x >= grid.length) {
+            return 0;
+        }
+        if (y < 0 || y >= grid[0].length) {
+            return 0;
+        }
         if (grid[x][y] != 1) {
             return 0;
         }
@@ -30,16 +35,49 @@ public class LeetCode0105 {
         for (int[] direction : directions) {
             int newX = x + direction[0];
             int newY = y + direction[1];
-            if (newX < 0 || newX >= grid.length) {
-                continue;
-            }
-            if (newY < 0 || newY >= grid[0].length) {
-                continue;
-            }
-            if (grid[newX][newY] == 1) {
-                result += count(grid, newX, newY);
+            result += dfs(grid, newX, newY);
+
+        }
+        return result;
+    }
+
+    public int maxAreaOfIslandStack(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int result = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int cur = dfsStack(grid, i, j);
+                result = Math.max(result, cur);
             }
         }
         return result;
+    }
+
+    private int dfsStack(int[][] grid, int i, int j) {
+        int cur = 0;
+        Deque<int[]> stack = new LinkedList<>();
+        stack.add(new int[]{i, j});
+        while (!stack.isEmpty()) {
+            int[] position = stack.pop();
+            int x = position[0];
+            int y = position[1];
+            if (x < 0 || x >= grid.length) {
+                continue;
+            }
+            if (y < 0 || y >= grid[0].length) {
+                continue;
+            }
+            if (grid[x][y] != 1) {
+                continue;
+            }
+            cur++;
+            grid[x][y] = 2;
+            for (int[] direction : directions) {
+                int[] newPosition = new int[] {x + direction[0], y + direction[1]};
+                stack.add(newPosition);
+            }
+        }
+        return cur;
     }
 }
