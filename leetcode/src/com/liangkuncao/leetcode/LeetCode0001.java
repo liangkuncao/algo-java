1b5d956230ff6e60174673b7e42b4f33f6b3b3a2
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2024-06-03 22:06:13 +0800

    leetcode 0001

--- leetcode/src/com/liangkuncao/leetcode/LeetCode0001.java
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0001.java
@@ -1,18 +1,32 @@
 package com.liangkuncao.leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class LeetCode0001 {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> seen = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (seen.containsKey(target - nums[i])) {
-                return new int[] {seen.get(target - nums[i]), i};
+                return new int[]{seen.get(target - nums[i]), i};
             } else {
                 seen.put(nums[i], i);
             }
         }
         return null;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
     }
 }
