137ca1481afc9c4640b6f4f882c9e8eaeca4252c
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2024-06-12 23:44:37 +0800

    leetcode 0035

--- leetcode/src/com/liangkuncao/leetcode/LeetCode0035.java
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0035.java
@@ -44,5 +44,25 @@ public class LeetCode0035 {
         return ans;
     }
 
-
+    public int searchInsert_20240612(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        if (target < nums[left]) {
+            return left;
+        }
+        if (target > nums[right]) {
+            return right + 1;
+        }
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
 }
