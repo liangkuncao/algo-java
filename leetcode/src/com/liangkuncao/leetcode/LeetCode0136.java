bf49ef624fecb9df5b9159721ac557b12db84a00
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2024-06-24 22:01:37 +0800

    leetcode 0136

--- leetcode/src/com/liangkuncao/leetcode/LeetCode0136.java
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0136.java
@@ -1,5 +1,8 @@
 package com.liangkuncao.leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LeetCode0136 {
     public int singleNumber(int[] nums) {
         int ret = 0;
@@ -7,5 +10,13 @@ public class LeetCode0136 {
             ret ^= num;
         }
         return ret;
+    }
+
+    public int singleNumber_20240624(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
     }
 }
