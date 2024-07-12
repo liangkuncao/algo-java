dd40c1cae4179e903ebf9d648f803bb2c98c0758
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2024-07-13 00:32:46 +0800

    leetcode 0094

---
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0094.java
@@ -0,0 +1,19 @@
+package com.liangkuncao.leetcode;
+
+import com.liangkuncao.leetcode.common.TreeNode
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode0094 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> result = new ArrayList<>();
+        result.addAll(inorderTraversal(root.left));
+        result.add(root.val);
+        result.addAll(inorderTraversal(root.right));
+        return result;
+    }
+}
