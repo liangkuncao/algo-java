acf5152e23e3fa31e2be90b9269ed2b6b1121987
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2024-07-16 22:10:03 +0800

    leetcode 0101

--- leetcode/src/com/liangkuncao/leetcode/LeetCode0101.java
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0101.java
@@ -1,8 +1,11 @@
 package com.liangkuncao.leetcode;
 
+import com.liangkuncao.leetcode.common.ListNode;
 import com.liangkuncao.leetcode.common.TreeNode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class LeetCode0101 {
@@ -56,6 +59,7 @@ public class LeetCode0101 {
      * 解法：递归
      * 时间复杂度：O（N），N是树的节点数
      * 空间复杂度：O（N），N是树的高度
+     *
      * @param root
      * @return
      */
@@ -80,4 +84,68 @@ public class LeetCode0101 {
                 && isSymmetric(left.right, right.left);
     }
 
+    /**
+     * 解法：递归
+     * 时间复杂度：O（N）
+     * 空间复杂度：O（N）
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric3(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric4(root.left, root.right);
+    }
+
+    private boolean isSymmetric4(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetric4(left.left, right.right) && isSymmetric4(left.right, right.left);
+    }
+
+    /**
+     * 解法：迭代
+     * 时间复杂度：O（N）
+     * 空间复杂度：O（N）
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric5(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Queue<TreeNode> leftRow = new LinkedList<>();
+        Queue<TreeNode> rightRow = new LinkedList<>();
+        leftRow.offer(root.left);
+        rightRow.offer(root.right);
+        while (!leftRow.isEmpty()) {
+            int size = leftRow.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode leftNode = leftRow.poll();
+                TreeNode rightNode = rightRow.poll();
+                if (leftNode == null && rightNode == null) {
+                    continue;
+                }
+                if (leftNode == null || rightNode == null) {
+                    return false;
+                }
+                if (leftNode.val != rightNode.val) {
+                    return false;
+                }
+                leftRow.offer(leftNode.left);
+                leftRow.offer(leftNode.right);
+                rightRow.offer(rightNode.right);
+                rightRow.offer(rightNode.left);
+            }
+        }
+        return true;
+    }
 }
