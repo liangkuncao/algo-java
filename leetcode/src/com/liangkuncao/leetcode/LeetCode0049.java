f10a3292e8eafa3b16ff84ac4384003ddd5accf6
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2024-07-19 22:16:38 +0800

    leetcode 0049

--- leetcode/src/com/liangkuncao/leetcode/LeetCode0049.java
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0049.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+import java.util.StringJoiner;
 public class LeetCode0049 {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> result = new HashMap<>();
@@ -28,5 +28,34 @@ public class LeetCode0049 {
             result.put(k, value);
         }
         return new ArrayList<>(result.values());
+    }
+
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            String key = getKey(str);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    private String getKey(String str) {
+        int[] letters = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            letters[ch - 'a']++;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < letters.length; i++) {
+            int count = letters[i];
+            char ch = (char) ('a' + i);
+            if (count > 0) {
+                stringBuilder.append(ch);
+                stringBuilder.append(count);
+            }
+        }
+        return stringBuilder.toString();
     }
 }
