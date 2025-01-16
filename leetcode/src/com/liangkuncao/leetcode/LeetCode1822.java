e55c1b6b7e8e0358ba5ae33a6d0d1544873ba208
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2025-01-16 08:16:19 +0800

    leetcode 1822

---
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode1822.java
@@ -0,0 +1,16 @@
+package com.liangkuncao.leetcode;
+
+public class LeetCode1822 {
+    public int arraySign(int[] nums) {
+        int sign = 1;
+        for (int num : nums) {
+            if (num == 0) {
+                return 0;
+            }
+            if (num < 0) {
+                sign = -sign;
+            }
+        }
+        return sign;
+    }
+}
