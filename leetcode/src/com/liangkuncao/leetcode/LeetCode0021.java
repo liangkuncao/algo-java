0b997047df62e3fa04fe5c4ed028aaf9ccd19b47
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2025-04-09 08:30:13 +0800

    2025/4/9

--- leetcode/src/com/liangkuncao/leetcode/LeetCode0021.java
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0021.java
@@ -96,6 +96,27 @@ public class LeetCode0021 {
         }
         return dummy.next;
     }
+
+    public ListNode mergeTwoLists20250409(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        while (list1 != null & list2 != null) {
+            if (list1.val <= list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        if (list1 == null) {
+            cur.next = list2;
+        } else {
+            cur.next = list1;
+        }
+        return dummy.next;
+    }
 }
 
 
--- leetcode/src/com/liangkuncao/leetcode/LeetCode0043.java
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0043.java
@@ -2,8 +2,7 @@ package com.liangkuncao.leetcode;
 
 public class LeetCode0043 {
     public String multiply(String num1, String num2) {
-        StringBuilder result = new StringBuilder();
-        int curSum = 0;
+        long curSum = 0;
         for (int i = num1.length() - 1; i >= 0; i--) {
             int n1 = num1.charAt(i) - '0';
             int carry = 0;
@@ -12,10 +11,10 @@ public class LeetCode0043 {
                 int total = n1 * n2 + carry;
                 carry = total / 10;
                 int curDigit = total % 10;
-                curSum += (int) (curDigit * Math.pow(10, i + j));
+                curSum += (int) (curDigit * Math.pow(10, num1.length() - i - 1 + num2.length() - j - 1));
             }
             if (carry > 0) {
-                curSum += (int) (carry * Math.pow(10, i + num2.length()));
+                curSum += (int) (carry * Math.pow(10, num1.length() - i - 1 + num2.length()));
             }
         }
         return String.valueOf(curSum);
