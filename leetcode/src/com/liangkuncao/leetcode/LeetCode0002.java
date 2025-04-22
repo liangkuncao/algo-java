3a38b5a54f49bb28ec974567403505c5b4e054a8
liangkuncao <50449296+liangkuncao@users.noreply.github.com>
2025-04-22 22:22:14 +0800

    2025/4/22

--- leetcode/src/com/liangkuncao/leetcode/LeetCode0002.java
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0002.java
@@ -22,4 +22,24 @@ public class LeetCode0002 {
         }
         return dummy.next;
     }
+
+    public ListNode addTwoNumbers20250414(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int num1 = l1 == null ? 0 : l1.val;
+            int num2 = l2 == null ? 0 : l2.val;
+            int total = num1 + num2 + carry;
+            carry = total / 10;
+            cur.next = new ListNode(total % 10);
+            cur = cur.next;
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+        }
+        if (carry > 0) {
+            cur.next = new ListNode(carry);
+        }
+        return dummy.next;
+    }
 }
---
+++ leetcode/src/com/liangkuncao/leetcode/LeetCode0026.java
@@ -0,0 +1,22 @@
+package com.liangkuncao.leetcode;
+
+import com.liangkuncao.leetcode.common.ListNode;
+
+public class LeetCode0026 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode next = head.next;
+        while (next != null) {
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+            next = next.next;
+        }
+        cur.next = pre;
+        return cur;
+    }
+}
